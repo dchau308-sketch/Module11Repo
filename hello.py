48f21386c10f7aa9122c3132c183dadba2a27ab6
dchau308-sketch <dchau308@gmail.com>
2026-08-15 23:17:40 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-# Firstname Lastname
+# Bao Chau Dang
 # 08/15/2026
 # A simple Python Program to print a message
 
+# Bao Chau Dang
+# 08/15/2026
+# Added new meesage
+
 print("Hello, World!")
+
+print("welcome to Github!")
